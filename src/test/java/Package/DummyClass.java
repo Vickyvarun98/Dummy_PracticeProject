8198f349cfa96433a034e40a997e3e35cb67d2ac
Vickyvarun98 <Vigneshwaransesuragavan@gmail.com>
2024-11-11 15:07:47 +0530

    package is added

---
+++ src/test/java/Package/DummyClass.java
@@ -0,0 +1,16 @@
+package Package;
+
+import org.testng.annotations.Test;
+
+public class DummyClass {
+
+	@Test
+	public void dummyMethod() {
+		
+		System.out.println("Hii");
+		System.out.println("Have a nice day");
+		
+	}
+	
+	
+}
