85339aaf9cd30fe166e87a368400521dfb6fd49c
Vickyvarun98 <Vigneshwaransesuragavan@gmail.com>
2024-11-11 15:22:37 +0530

    New class is added.....

---
+++ src/test/java/Package/NewClass.java
@@ -0,0 +1,14 @@
+package Package;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+
+	@Test
+	public void newClass() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello");
+		}
+
+	}
+}
