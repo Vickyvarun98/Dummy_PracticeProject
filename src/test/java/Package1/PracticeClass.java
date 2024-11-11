9a85fe93ef0d3248ad8447807cb198ceca231dc9
Milky34 <vigneshwaransesuragavan@gmail.com>
2024-11-11 15:52:11 +0530

    Changes done

---
+++ src/test/java/Package1/PracticeClass.java
@@ -0,0 +1,13 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass {
+	
+	@Test
+	public void prectiseSession() {
+		
+		System.out.println("Good Evening...");
+	}
+
+}
