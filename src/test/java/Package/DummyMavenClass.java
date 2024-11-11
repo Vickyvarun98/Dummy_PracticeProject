b6ab50a3805f553520c96b96f05b99eab68847a0
Vickyvarun98 <Vigneshwaransesuragavan@gmail.com>
2024-11-11 15:14:42 +0530

    one Dummyclass is added

---
+++ src/test/java/Package/DummyMavenClass.java
@@ -0,0 +1,12 @@
+package Package;
+
+import org.testng.annotations.Test;
+
+public class DummyMavenClass {
+	
+	@Test
+	public void practiceClass() {
+		System.out.println("Practising maven class....");
+	}
+
+}
