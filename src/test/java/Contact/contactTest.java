7f083c4e12c53948201ea4e71cb0cd94ccdb22e2
Vickyvarun98 <vigneshwaransesuragavan@gmail.com>
2024-11-13 12:53:10 +0530

    Details are added

--- src/test/java/Contact/contactTest.java
+++ src/test/java/Contact/contactTest.java
@@ -3,10 +3,26 @@ package Contact;
 import org.testng.annotations.Test;
 
 public class contactTest {
+	
+	
+	
+	@Test(groups = {"smoke","regression"})
+	public void getData() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+	}
 
 	@Test(groups = "smoke")
 	public void smokeTest() {
 		System.out.println("Smoke Test for contact is executed.....");
+		
 		
 	}
 	@Test(groups = "regression")
