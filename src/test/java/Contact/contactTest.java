906cbaf4baf8062bf8e5789c8fe84dd325e22b81
Vickyvarun98 <vigneshwaransesuragavan@gmail.com>
2024-11-13 13:10:15 +0530

    updateed

--- src/test/java/Contact/contactTest.java
+++ src/test/java/Contact/contactTest.java
@@ -8,14 +8,15 @@ public class contactTest {
 	
 	@Test(groups = {"smoke","regression"})
 	public void getData_Test() {
-		String URL = System.getProperty("url");
-		String BROWSER = System.getProperty("browser");
-		String USERNAME = System.getProperty("username");
-		String PASSWORD = System.getProperty("password");
-		System.out.println(URL);
-		System.out.println(BROWSER);
-		System.out.println(USERNAME);
-		System.out.println(PASSWORD);
+//		String URL = System.getProperty("url");
+//		String BROWSER = System.getProperty("browser");
+//		String USERNAME = System.getProperty("username");
+//		String PASSWORD = System.getProperty("password");
+//		System.out.println(URL);
+//		System.out.println(BROWSER);
+//		System.out.println(USERNAME);
+//		System.out.println(PASSWORD);
+		System.out.println("Hii");
 		
 	}
 
